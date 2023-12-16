4cbbc95f63efe35b3149a01cd6906f4cafb6a39d
Alex1Go <125027886+Alex1Go@users.noreply.github.com>
2023-12-16 12:01:09 +0200

    create repo

---
+++ validation/contactValidationSchemas.js
@@ -0,0 +1,13 @@
+const Joi = require("joi");
+
+const schema = Joi.object({
+  name: Joi.string().min(3).max(30).required(),
+  email: Joi.string().email({
+    minDomainSegments: 2,
+    tlds: { allow: ["com", "net"] },
+  }),
+  phone: Joi.string().required(),
+  favorite: Joi.boolean(),
+});
+
+module.exports = { schema };
