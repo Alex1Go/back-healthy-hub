274a688afb489728da3fedaa5acc973c7a9a6da2
Alex1Go <125027886+Alex1Go@users.noreply.github.com>
2023-12-20 22:27:18 +0200

    add swigger

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 require("./db");
 
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
@@ -16,6 +18,7 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
 app.use("/api/", recommededRouter);
