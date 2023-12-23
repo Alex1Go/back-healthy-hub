db77f7ec5b30ec017c71f3db08845a913025866e
Alex1Go <125027886+Alex1Go@users.noreply.github.com>
2023-12-23 19:29:19 +0200

    add

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,15 +1,15 @@
 const User = require("../models/users");
 const UserCard = require("../models/userCard");
 const bcrypt = require("bcrypt");
-const nanoid = require("nanoid");
+// const nanoid = require("nanoid");
 const jwt = require("jsonwebtoken");
 const { bmrCalc, waterCalc, ratioCalc } = require("../helpres/calculation");
 const {
   registrSchema,
   loginSchema,
-  forgotSchema,
+  // forgotSchema,
 } = require("../validation/userValidationSchema");
-const { sendEmail } = require("../helpres/sendEmail");
+// const { sendEmail } = require("../helpres/sendEmail");
 
 async function signup(req, res, next) {
   const {
@@ -158,35 +158,35 @@ async function signout(req, res, next) {
 }
 
 async function forgotPassword(req, res, next) {
-  const { email } = req.body;
-  try {
-    const { error } = forgotSchema.validate({ email });
-    if (error) {
-      return res.status(400).json({ message: "Invalid email format" });
-    }
-    const newPassword = nanoid();
-    const user = await User.findOne({ email });
-    if (!user) {
-      return res.status(422).json({ message: "User doesn't exist!" });
-    }
-    const hashPassword = await bcrypt.hash(newPassword, 10);
-    user.password = hashPassword;
-    user.verificationToken = nanoid();
-    user.verify = true;
-    await user.save();
-    const emailContent = {
-      to: email,
-      subject: "New password",
-      html: `<h2>Your new password:</h2> 
-      <p> ${newPassword}</p>`,
-    };
-    await sendEmail(emailContent);
-    res.json({
-      message: "New password sent to your email",
-    });
-  } catch (error) {
-    next(error);
-  }
+  // const { email } = req.body;
+  // try {
+  //   const { error } = forgotSchema.validate({ email });
+  //   if (error) {
+  //     return res.status(400).json({ message: "Invalid email format" });
+  //   }
+  //   const newPassword = nanoid();
+  //   const user = await User.findOne({ email });
+  //   if (!user) {
+  //     return res.status(422).json({ message: "User doesn't exist!" });
+  //   }
+  //   const hashPassword = await bcrypt.hash(newPassword, 10);
+  //   user.password = hashPassword;
+  //   user.verificationToken = nanoid();
+  //   user.verify = true;
+  //   await user.save();
+  //   const emailContent = {
+  //     to: email,
+  //     subject: "New password",
+  //     html: `<h2>Your new password:</h2>
+  //     <p> ${newPassword}</p>`,
+  //   };
+  //   await sendEmail(emailContent);
+  //   res.json({
+  //     message: "New password sent to your email",
+  //   });
+  // } catch (error) {
+  //   next(error);
+  // }
 }
 
 module.exports = { signup, signin, signout, forgotPassword };
